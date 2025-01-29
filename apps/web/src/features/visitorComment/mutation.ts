b21a5b4e25b32bc47323ff260038afba818a7887
15077693d <oscaryiu.lapsang@gmail.com>
2025-01-30 00:38:51 +0800

    refactor: Update visitor comment mutation type import

--- apps/web/src/features/visitorComment/mutation.ts
+++ apps/web/src/features/visitorComment/mutation.ts
@@ -1,12 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { CreateVisitorCommentDto } from 'database'
+import { CreateVisitorComment } from 'database'
 import { apiClient, QUERY_KEYS } from '../../constants/query'
 
 export const usePostComment = () => {
     const queryClient = useQueryClient()
 
     return useMutation({
-        mutationFn: (commentData: CreateVisitorCommentDto) =>
+        mutationFn: (commentData: CreateVisitorComment) =>
             apiClient.post('/visitor-comment', commentData),
         onSuccess: () => {
             queryClient.invalidateQueries({
