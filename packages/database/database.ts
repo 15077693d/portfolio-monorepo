e5c0a6c8b672fdf030dc14126137593118f87e64
15077693d <oscaryiu.lapsang@gmail.com>
2025-01-31 00:37:05 +0800

    chore: Adjust .env file path for API and database modules

--- packages/database/database.ts
+++ packages/database/database.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import * as schema from "./drizzle/schema";
-require("dotenv").config({ path: "../../../.env" });
+require("dotenv").config({ path: "../../.env" });
 import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL || "";
