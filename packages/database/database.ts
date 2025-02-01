a1ae957ff1f2581f8e0ba538d40c420256eba6b1
15077693d <oscaryiu.lapsang@gmail.com>
2025-02-01 21:42:37 +0800

    chore: Remove redundant Turbo task dependencies and dotenv configuration

--- packages/database/database.ts
+++ packages/database/database.ts
@@ -3,7 +3,6 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import * as schema from "./drizzle/schema";
 require("dotenv").config({ path: "../../.env" });
-require("dotenv").config();
 import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL || "";
